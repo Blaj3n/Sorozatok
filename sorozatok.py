7d08298a3c4dbde5cd3df805e7144ed29147a752
Blaj3n <benceulrich@gmail.com>
2025-03-20 14:58:27 +0100

    1. feladat fájl beolvasása dictionarybe

---
+++ sorozatok.py
@@ -0,0 +1,23 @@
+# print("1. feladat")
+# dictionary key:value
+egy_epizod = {} # egy epizód adatait mutatja
+epizodok = [] # az összes epizód
+
+adatok = []
+with open("lista.txt", "r", encoding="utf-8") as file:
+    for egysor in file:
+        adatok.append(egysor.strip())
+        if len(adatok) == 5:
+            egy_epizod["datum"] = adatok[0]
+            egy_epizod["nev"] = adatok[1]
+            egy_epizod["resz"] = adatok[2]
+            egy_epizod["hossz"] = adatok[3]
+            if adatok[4] == "1":
+                egy_epizod["latta"] = True
+            else:
+                egy_epizod["latta"] = False
+
+            epizodok.append(egy_epizod)
+            egy_epizod = {}
+            adatok = []
+print(epizodok)
